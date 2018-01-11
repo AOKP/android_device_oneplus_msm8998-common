cfc6897f4956889afacd3044d996b5f8a13a07cf
villarleg <santialovera20@gmail.com>
2018-01-11 22:01:29 +0100

    Partially Revert "Revert "msm8998: Enable boot and system server dex-preopt""
    This reverts commit eec2f1646b713048894734932aa66a8b6efcfa7c.
    
    Was causing the build fail, just enable witouth changing the path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -202,6 +202,7 @@ ifeq ($(HOST_OS),linux)
   ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
   endif
 endif
