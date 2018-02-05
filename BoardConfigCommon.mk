d1ab4736cae71c84637c6c034e96904df01341b2
villarleg <santialovera20@gmail.com>
2018-02-05 15:19:14 +0100

    msm8998-common: Fix more denials

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,15 +200,7 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
+
 
 # GPS
 TARGET_NO_RPC := true
